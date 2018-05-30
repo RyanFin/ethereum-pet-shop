963a2e08493f9163eddaac3eae9769c7151c7c1c
Ryan Finlayson <Ryanfinlayson125@hotmail.com>
2018-05-30 18:20:46 +0100

    first function complete

---
+++ README.md
@@ -0,0 +1,9 @@
+# ethereum-pet-shop
+https://guides.github.com/features/mastering-markdown/
+
+## useful links
+**Main Tutorial:** http://truffleframework.com/tutorials/pet-shop
+
+Miscellaneous: 
+* http://truffleframework.com/docs/getting_started/project
+* http://truffleframework.com/ganache/
---
+++ contracts/Adoption.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.17;
+
+contract Adoption {
+
+//create array of Ethereum Addresses
+address[16] public adopters; //homogeneous array with a fixed length of 16 
+
+// Adopting a pet, parameter (int) returns (int)
+function adopt(uint petId) public returns (uint) {
+
+    require(petId >= 0 && petId <= 15); //ensure that the petId is within the range of adopters array (starts from 0)
+
+    adopters[petId] = msg.sender; //Add address of the person / smart contract that made the call (denoted by msg.sender) to the adopters array
+
+    return petId; //return an integer (as function specified) as a confirmation
+}
+
+}
